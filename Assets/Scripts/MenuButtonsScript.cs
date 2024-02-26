0b035f38a16477d865157d0c1548645459187cef
WillBoudreau <williamboudreau804@gmail.com>
2024-02-26 12:12:08 -0400

    Added PNG +JPEG images

--- Assets/Scripts/MenuButtonsScript.cs
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -2,26 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class MenuButtonsScript : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
     public int score;
    public void ButtonManager( int chooser)
     {
         if(chooser == 0)
         {
             score += 15;
-            Debug.Log("Score " + score);
+            Debug.Log("Score " + score.ToString());
+            scoreText.text = "Score: " + score.ToString();
         }
         if(chooser == 1)
         {
             score -= 15;
-            Debug.Log("Score " + score);
+            if(score < 0)
+            {
+                score = 0;
+            }
+            Debug.Log("Score " + score.ToString());
+            scoreText.text = "Score: " + score.ToString();
         }
         if(chooser == 2)
         {
             score = 0;
-            Debug.Log("Score" + score);
+            Debug.Log("Score" + score.ToString());
+            scoreText.text = "Score: " + score.ToString();
         }
     }
 }
