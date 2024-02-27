b9b7815e57281fb7d2b291cfe718c8f9cbb7ccab
WillBoudreau <williamboudreau804@gmail.com>
2024-02-27 10:19:38 -0400

    Sliders work

--- Assets/Scripts/MenuButtonsScript.cs
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -8,9 +8,15 @@ public class MenuButtonsScript : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public int score;
-    public void UpdateScore( int value)
+    int maxscore = 999999999;
+    int minscore = 0;
+
+    public void RandomNum()
     {
-        
+
+    }
+    public void UpdateScore(int value)
+    {
         if(value == 0)
         {
             score = value;
@@ -18,6 +24,14 @@ public class MenuButtonsScript : MonoBehaviour
         else if(value != 0) 
         {
             score += value;
+            if(score < 0 )
+            {
+                score = 0;
+            }
+            else if (score > maxscore)
+            {
+                score = maxscore;
+            }
         }
         scoreText.text = "Score: " + score.ToString();
     }
---
+++ Assets/Scripts/SliderBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderBehaviour : MonoBehaviour
+{
+    public Slider rSlider;
+    public Slider gSlider;
+    public Slider bSlider;
+    public Slider aSlider;
+    public float sliderValue;
+    public Image sliderImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rSlider.value = 1;
+        gSlider.value = 1;
+        bSlider.value = 1;
+        aSlider.value = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(rSlider.value);
+        sliderImage.color = new Color(bSlider.value,rSlider.value,gSlider.value,aSlider.value);
+    }
+}
